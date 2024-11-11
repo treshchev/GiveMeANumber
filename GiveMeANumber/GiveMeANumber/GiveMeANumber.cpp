99c6d6a0ef54cde5f5a10e713e38d38cd25619b6
Vladislav Treshchev <letsgovlad@gmail.com>
2024-11-11 20:45:33 +0300

    Created project

---
+++ GiveMeANumber/GiveMeANumber/GiveMeANumber.cpp
@@ -0,0 +1,28 @@
+// Program Give Me A Number
+// Describes work with arguments of functions, that set by default
+
+#include <iostream>
+#include <string>
+
+int askNumber(int high, int low = 1);
+int main()
+{
+    int number = askNumber(5);
+    std::cout << "Thanks for entering: " << number << "\n\n";
+    
+    number = askNumber(10, 5);
+    std::cout << "Thanks for entering: " << number << "\n\n";
+
+    return 0;
+}
+int askNumber(int high, int low)
+{
+    int num;
+    do
+    {
+        std::cout << "Please enter a number: " << " (" << high << " - " << low << "): ";
+        std::cin >> num;
+        
+    } while (num > high || num < low);
+    return num;
+}
